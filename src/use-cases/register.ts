186d1a12e9311d3d300ac16fc519272229e4fccf
“tiagoluis12” <tiago7164@hotmail.com>
2023-11-28 19:19:30 +0000

    Use-case Design patterns: Erros use-case

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,6 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './erros/user-already-exists-error'
 
 interface registerUseCaseRequest {
   name: string
@@ -16,7 +17,7 @@ export class RegisterUseCase {
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
-      throw new Error('User already exists')
+      throw new UserAlreadyExistsError()
     }
 
     await this.usersRepository.create({
