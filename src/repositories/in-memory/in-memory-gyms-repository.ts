9ee54e3089e388a7da5ac349c75e01a9699e16ba
“tiagoluis12” <tiago7164@hotmail.com>
2024-07-24 13:25:28 +0100

    feat: Implementing Use Cases – Nearby Gyms Use Case

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,4 +1,5 @@
-import { GymsRepository } from '@/repositories/gyms-repository'
+import { GymsRepository, FindManyNearbyParams } from '@/repositories/gyms-repository'
+import { getDistanceBetweenCordinates } from '@/utils/get-distance-between-coordinates'
 import { Gym, Prisma } from '@prisma/client'
 import { randomUUID } from 'node:crypto'
 
@@ -13,6 +14,26 @@ export class InMemoryGymsRepository implements GymsRepository {
     }
 
     return gym
+  }
+
+  async findManyNearby(params: FindManyNearbyParams){
+    return this.items.filter((item) => {
+      const distance = getDistanceBetweenCordinates(
+        { latitude: params.latitude, longitude: params.longitude },
+        {
+          latitude: item.latitude.toNumber(),
+          longitude: item.longitude.toNumber(),
+        },
+      )
+
+      return distance < 10
+    })
+  }
+
+  async searchMany(query: string, page: number) {
+    return this.items
+    .filter((item) => item.title.includes(query))
+    .slice((page -1) * 20, page * 20)
   }
 
   async create(data: Prisma.GymCreateInput) {
